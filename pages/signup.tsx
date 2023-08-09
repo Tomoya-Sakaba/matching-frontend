b7cd5bda851ec00758496ee56fdcee32c1c1e9c0
ippei0110 <ippei.0110.s@gmail.com>
2023-08-09 15:32:35 +0900

    test

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://localhost:80";
 
 const Signup: NextPage = () => {
     const router = useRouter();
